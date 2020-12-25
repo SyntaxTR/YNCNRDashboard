3814a437884c9c1a7856e41cb673c2a3159ede3f
SyntaxTR <61349074+SyntaxTR@users.noreply.github.com>
2020-12-25 13:45:05 +0300

    OnlinePlayers
    Trash codes removed.

--- app/src/main/java/com/yalovaedu/yncnrdashboard/IndexActivity.java
+++ app/src/main/java/com/yalovaedu/yncnrdashboard/IndexActivity.java
@@ -1,9 +1,5 @@
 package com.yalovaedu.yncnrdashboard;
 
-<<<<<<< Updated upstream
-
-=======
->>>>>>> Stashed changes
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -20,35 +16,12 @@ import com.huawei.hms.ads.AdParam;
 import com.huawei.hms.ads.banner.BannerView;
 
 public class IndexActivity extends AppCompatActivity implements View.OnClickListener{
-<<<<<<< Updated upstream
-
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
-import android.util.Log;
-import android.widget.FrameLayout;
-import android.widget.LinearLayout;
-import android.widget.Toast;
-
-import com.huawei.hms.ads.AdListener;
-import com.huawei.hms.ads.AdParam;
-import com.huawei.hms.ads.BannerAdSize;
-import com.huawei.hms.ads.banner.BannerView;
-
-public class IndexActivity extends AppCompatActivity {
-
-=======
->>>>>>> Stashed changes
     BannerView bannerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_index);
         // Obtain BannerView.
-<<<<<<< Updated upstream
-
-=======
->>>>>>> Stashed changes
         bannerView = findViewById(R.id.hw_banner_view);
         AdParam adParam = new AdParam.Builder().build();
         bannerView.loadAd(adParam);
@@ -72,45 +45,4 @@ public class IndexActivity extends AppCompatActivity {
             }
         }
     }
-<<<<<<< Updated upstream
-
-        BannerView bannerView = findViewById(R.id.hw_banner_view);
-        bannerView.setAdListener(adListener);
-        AdParam adParam = new AdParam.Builder().build();
-        bannerView.loadAd(adParam);
-    }
-    private AdListener adListener = new AdListener() {
-        @Override
-        public void onAdLoaded() {
-            super.onAdLoaded();
-        }
-
-        @Override
-        public void onAdFailed(int errorCode) {
-            Toast.makeText(IndexActivity.this, "Ad load failed with error code: " + errorCode,
-                    Toast.LENGTH_SHORT).show();
-            Log.d("ERROR", "Ad load failed with error code: " + errorCode);
-        }
-
-        @Override
-        public void onAdClosed() {
-            super.onAdClosed();
-            Log.d("TAG", "onAdClosed");
-        }
-
-        @Override
-        public void onAdClicked() {
-            Log.d("TAG", "onAdClicked");
-            super.onAdClicked();
-        }
-
-        @Override
-        public void onAdOpened() {
-            Log.d("TAG", "onAdOpened");
-            super.onAdOpened();
-        }
-    };
-
-=======
->>>>>>> Stashed changes
 }
